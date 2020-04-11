7ff96aa3e255a443807adc2ff7efc37484a34d6a
KevinTellier2 <54898019+KevinTellier2@users.noreply.github.com>
2020-04-11 10:51:01 +0200

    Update projet_cifar10.py

--- projet_cifar10.py
+++ projet_cifar10.py
@@ -678,3 +678,39 @@ plt.imshow(test_image_1)
 Conclusion :
 Eight images out ten correctly predicted on collected images.
 '''
+
+
+
+# Obviously, if you want to try with you own images, you can use the following code, replacing PATH.JPG by your path to the JPG image :
+'''
+test_image_1 = image.load_img('PATH.JPG', target_size = (32, 32))
+test_image = image.img_to_array(test_image_1)
+test_image = np.expand_dims(test_image, axis = 0) 
+result = model.predict(test_image) 
+print(result) 
+if result[0][0]==1: 
+    print("Airplane") 
+elif result[0][1]==1: 
+    print('Automobile') 
+elif result[0][2]==1: 
+    print('Bird') 
+elif result[0][3]==1: 
+    print('Cat') 
+elif result[0][4]==1: 
+    print('Deer') 
+elif result[0][5]==1: 
+    print('Dog') 
+elif result[0][6]==1: 
+    print('Frog') 
+elif result[0][7]==1: 
+    print('Horse') 
+elif result[0][8]==1: 
+    print('Ship') 
+elif result[0][9]==1: 
+    print('Truck') 
+else: 
+    print('Error')
+
+plt.imshow(test_image_1)
+# Is your image correctly predicted ?
+'''
